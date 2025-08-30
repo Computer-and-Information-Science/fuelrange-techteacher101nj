164067bd7712ddedbb4c8700d4b7a470ad033825
Lisa Waddell <90416736+techteacher101nj@users.noreply.github.com>
2025-08-30 15:58:23 +0000

    done

--- main.cpp
+++ main.cpp
@@ -2,14 +2,19 @@
 // TODO: YOUR NAME HERE
 #include <iostream>
 // TODO: Add the "using" statement
-
+using namespace std;
 int main() {
   // TODO: Declare your variables, with comments for each
+  float capacity =0;
+  float mpg = 0;
+  int percentFull = 0;
+  float milesLeft = 0;
 
   // TODO: Prompt for and input the required values
-
+cout << " Please input the capacity of your fuel tank, the mpg, and the percent filled. " << endl;
+cin >> capacity >> mpg >> percentFull;
   // TODO: Calculate the results
-
+milesLeft = (capacity * percentFull)* mpg /100;
   // TODO: Output the results
-  
+  cout << "You can travel : " << milesLeft << " miles.";
 }
